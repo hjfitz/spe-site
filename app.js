01bfd6d19e940b8ba49ee3db1b8d35ed630e4d7c
Harry <hfitz83@gmail.com>
2017-10-07 15:22:08 +0100

    cleanup

--- app.js
+++ app.js
@@ -6,16 +6,13 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 const renderer = require('./routes/handlebars');
-const contentfulAPI = require('./routes/contentful');
 
 app.engine('handlebars', hbs({ defaultLayout: 'layout' }));
 app.set('view engine', 'handlebars');
 
-app.use('/contentful', contentfulAPI);
 
 app.use('/public', express.static(path.join(__dirname, 'public')));
 app.use('/', renderer);
-// app.use('/', express.static(path.join(__dirname, 'webpages')));
 
 app.use('/', (req, res, next) => {
 	console.log(new Date(), req.method, req.url);
